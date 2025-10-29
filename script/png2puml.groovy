cc86020ac3fc4fdffc656aea8deed456b80c7dd1
mym <yaroslav@mizgirev.ru>
2025-10-29 20:47:31 +0300

    Update png2puml

--- script/png2puml.groovy
+++ script/png2puml.groovy
@@ -1,25 +1,23 @@
 #!/usr/bin/env groovy
 @Grab('net.sourceforge.plantuml:plantuml:1.2023.8')
-// @Grab('org.apache.xmlgraphics:batik-transcoder:1.16')
-// @Grab('org.apache.xmlgraphics:batik-codec:1.16')
 
 import groovy.cli.commons.CliBuilder
 import net.sourceforge.plantuml.klimt.sprite.SpriteGrayLevel
 import net.sourceforge.plantuml.klimt.sprite.SpriteUtils
-// import org.apache.batik.transcoder.TranscoderInput
-// import org.apache.batik.transcoder.TranscoderOutput
-// import org.apache.batik.transcoder.image.PNGTranscoder
 
 import javax.imageio.ImageIO
 import java.awt.*
 import java.awt.image.BufferedImage
 
+final DEFAULT_SCALE = 1
+final TMP_DIR = new File('/tmp/pngFile2plantUmlSprite')
+TMP_DIR.mkdirs()
 final DEFAULT_OUTPUT_DIR = new File('.')
 
 def cli = new CliBuilder(usage: "${this.class.getSimpleName()}.groovy [options] <png file>", stopAtNonOption: false, footer: "Usage example: ./${this.class.getSimpleName()}.groovy logo.png")
 cli.o(longOpt: 'output-dir', args: 1, argName: 'output dir', "Output directory for PUML file. Default: current directory")
-// cli.p(longOpt: 'png-dir', args: 1, argName: 'png dir', "Output directory for PNG files (overrides --output-dir for PNG)")
-cli.s(longOpt: 'sprite-name', args: 1, argName: 'sprite name', "Name for the sprite (default: derived from SVG filename)")
+cli.n(longOpt: 'sprite-name', args: 1, argName: 'sprite name', "Name for the sprite (default: derived from PNG filename)")
+cli.s(longOpt: 'scale', args: 1, argName: 'scale', "Scale of sprites. Default value: $DEFAULT_SCALE")
 
 def options = cli.parse(args)
 !options && System.exit(1)
@@ -43,104 +41,59 @@ if (!pngFile.exists()) {
 println("Processing PNG file: ${pngFile}")
 
 def outputDir = options.o ? new File(options.o) : DEFAULT_OUTPUT_DIR
-// def pngDir = options.p ? new File(options.p) : outputDir
-
 outputDir.mkdirs()
-// pngDir.mkdirs()
-// println("PNG output directory: ${pngDir}")
 println("PUML output directory: ${outputDir}")
 
-def spriteName = options.s ?: removeExtension(pngFile.name)
+def spriteName = options.n ?: removeExtension(pngFile.name)
 println("Sprite name: ${spriteName}")
 
-// def pngFile = svg2Png(svgFile, pngDir)
-// println("Created PNG: ${pngFile}")
+def scaleFactor = parseScaleOption(options.s, DEFAULT_SCALE)
+
+static def parseScaleOption(scaleOption, defaultValue) {
+    if (!scaleOption) return defaultValue
+    
+    try {
+        def scale = scaleOption.toDouble()
+        if (scale <= 0) {
+            println "Error: ${scaleOption} must be positive. Using default: $defaultValue"
+            return defaultValue
+        }
+        return scale
+    } catch (NumberFormatException | IllegalArgumentException e) {
+        println "Error: ${e.message}. Using default: $defaultValue"
+        return defaultValue
+    }
+}
+
+println("Scale value: ${scaleFactor}")
+
+if (scaleFactor != 1) {
+    pngFile = scaleImage(pngFile, scaleFactor, TMP_DIR)
+}
 
 def pumlFile = png2PlantUmlSprite(pngFile, outputDir, spriteName)
 println("Created PlantUML sprite: ${pumlFile}")
 
+deleteTempFolder(TMP_DIR)
+
 println("Conversion completed successfully!")
 
-// static def svg2Png(svgFile, outputDir) {
-//     def fileName = replaceExtension(svgFile.name, "png")
-//     def pngFile = new File(outputDir, fileName)
-    
-//     // Получаем реальные размеры из SVG
-//     def dimensions = getSVGRealDimensions(svgFile)
-//     println "SVG real dimensions: ${dimensions.width}x${dimensions.height}"
-
-//     OutputStream pngOut = new FileOutputStream(pngFile)
-//     try {
-//         PNGTranscoder transcoder = new PNGTranscoder()
-        
-//         // ЯВНО устанавливаем реальные размеры
-//         transcoder.addTranscodingHint(PNGTranscoder.KEY_WIDTH, dimensions.width as float)
-//         transcoder.addTranscodingHint(PNGTranscoder.KEY_HEIGHT, dimensions.height as float)
-
-//         transcoder.transcode(new TranscoderInput(svgFile.toURI().toString()), new TranscoderOutput(pngOut))
-//     } finally {
-//         pngOut.close()
-//     }
-
-//     // Проверяем результат
-//     def bufferedImage = ImageIO.read(pngFile)
-//     println "PNG result dimensions: ${bufferedImage.width}x${bufferedImage.height}"
-
-//     return pngFile
-// }
-
-// static def getSVGRealDimensions(svgFile) {
-//     def svgText = svgFile.text
-    
-//     // Парсим viewBox - это самый надежный способ получить реальные размеры
-//     def viewBoxMatch = (svgText =~ /viewBox\s*=\s*"([^"]+)"/)
-//     def widthMatch = (svgText =~ /width\s*=\s*"([^"]+)"/)
-//     def heightMatch = (svgText =~ /height\s*=\s*"([^"]+)"/)
-    
-//     float width = 0
-//     float height = 0
-    
-//     // Приоритет 1: viewBox (самый точный)
-//     if (viewBoxMatch) {
-//         def viewBoxParts = viewBoxMatch[0][1].split("\\s+")
-//         if (viewBoxParts.size() >= 4) {
-//             width = parseSVGUnit(viewBoxParts[2])
-//             height = parseSVGUnit(viewBoxParts[3])
-//             println "Using viewBox dimensions: ${width}x${height}"
-//         }
-//     }
-    
-//     // Приоритет 2: явные width/height
-//     if (width <= 0 && widthMatch) {
-//         width = parseSVGUnit(widthMatch[0][1])
-//     }
-//     if (height <= 0 && heightMatch) {
-//         height = parseSVGUnit(heightMatch[0][1])
-//     }
-    
-//     // Приоритет 3: дефолтные значения
-//     if (width <= 0) {
-//         width = 100
-//         println "Using default width: 100"
-//     }
-//     if (height <= 0) {
-//         height = 100
-//         println "Using default height: 100"
-//     }
-    
-//     return [width: width, height: height]
-// }
-
-// static def parseSVGUnit(value) {
-//     if (!value) return 0
-    
-//     // Убираем единицы измерения и преобразуем в число
-//     def numericValue = value.replaceAll("[^0-9.]", "")
-//     if (numericValue.isNumber()) {
-//         return numericValue.toFloat()
-//     }
-//     return 0
-// }
+static def scaleImage(pngFile, scaleFactor, workDir) {
+    BufferedImage im = ImageIO.read(pngFile)
+    def width = (im.width * scaleFactor) as Integer
+    def height = (im.height * scaleFactor) as Integer
+    BufferedImage scaledImage = new BufferedImage(width, height, im.type)
+    Graphics2D graphics2D = scaledImage.createGraphics()
+    graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR)
+    graphics2D.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
+    graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    graphics2D.drawImage(im, 0, 0, width, height, null)
+    graphics2D.dispose()
+    def fileName = pngFile.name
+    def newPngFile = new File(workDir, fileName)
+    ImageIO.write(scaledImage, 'png', newPngFile)
+    return newPngFile
+}
 
 static def replaceExtension(filename, newExtension) {
     int lastDotIndex = filename.lastIndexOf('.')
@@ -178,5 +131,19 @@ static def removeAlpha(im) {
         graphics.fillRect(0, 0, im.getWidth(), im.getHeight())
     } finally {
         graphics.dispose()
+    }
+}
+
+def deleteTempFolder(File folder) {
+    if (folder.exists()) {
+        folder.eachFile { file ->
+            if (file.isDirectory()) {
+                deleteTempFolder(file)
+            } else {
+                file.delete()
+            }
+        }
+        folder.delete()
+        println "Temp folder deleted: ${folder.absolutePath}"
     }
 }
